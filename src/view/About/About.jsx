74fe8955b5e4d77d1d17c37bd8a96fd6f4f65134
AshishSMathpati <ashishsmathpati@gmail.com>
2026-01-06 20:26:30 +0530

    more box added

--- src/view/About/About.jsx
+++ src/view/About/About.jsx
@@ -1,22 +1,24 @@
-import Who from "./../../images/group.png"
-import Why from "./../../images/why-choose.png"
-import Home from "./../../images/about-home-demo.jpg"
+import Who from "./../../images/about/group.png"
+import Why from "./../../images/about/why-choose.png"
+import Home from "./../../images/about/about-home-demo.jpg"
+import OurVision from "./../../images/about/vision.jpg"
+import ContactUs from "./../../images/about/contact-us.jpg"
 function About() {
     return <>
         <div>
-            <h1 className="text-6xl py-5">About Page</h1>
-            <img src={Home} className="h-110 w-220 mx-45 " />
+            <h1 className="text-6xl py-5 text-center">About Page</h1>
+            <img src={Home} className="h-110 w-220 mx-45 shadow-xl/30 shadow-red-500/50   " />
 
 
 
-            <div className="border-1 rounded-2xl my-10 mx-50 bg-red 500 max-w-220
-             flex flex-row gap-5 ">
+            <div className="border-1 rounded-2xl my-10 mx-5 bg-red 500 max-w-220
+             flex flex-row gap-5 justify-self-end ">
 
                 <img src={Who} className="w-60 h-60 my-10 mx-5 item-left" />
 
                 <div className="item-right flex flex-col p-5 bg-red-800/50">
 
-                    <h1 className="text-3xl font-extrabold p-5">Who We Are</h1>
+                    <h1 className="text-3xl font-extrabold p-5 text-center">Who We Are</h1>
                     <p>Welcome to Real Estate, a trusted name in the real estate industry committed
                         to helping individuals, families, and investors find the perfect property. With a strong
                         understanding of the local market and a passion for excellence, we deliver transparent,
@@ -29,13 +31,30 @@ function About() {
 
             </div>
 
-            <div className="border-1 rounded-2xl my-10 mx-50 bg-red 500 max-w-220
-             flex flex-row gap-5 ">
+            <div className="border-1 rounded-2xl my-10 mx-5 bg-red 500 max-w-250
+             flex flex-row gap-5 justify-self-start">
                 <img src={Why} className="w-60 h-60 my-10 mx-5" />
 
                 <div className="item-right flex flex-col p-5 bg-red-800/50">
 
-                    <h1 className="text-3xl font-extrabold p-5">Who We Are</h1>
+                    <h1 className="text-3xl font-extrabold p-5 text-center">Why choose US ?</h1>
+                    <p>
+                        1. Our team consists of skilled real estate experts with in-depth market knowledge. <br />
+                        2. No hidden charges, clear communication, and honest pricing. <br />
+                        3. Your needs and goals always come first. <br />
+                        4. Strong understanding of neighborhood trends and property values <br />
+                        5. From property search to final handover, we are with you at every step. <br />
+                    </p>
+                </div>
+            </div>
+
+            <div className="border-1 rounded-2xl my-10 mx-5 bg-red 500 max-w-220
+             flex flex-row gap-5 justify-self-end">
+                <img src={OurVision} className="w-80 h-60 my-10 mx-5" />
+
+                <div className="item-right flex flex-col p-5 bg-red-800/50">
+
+                    <h1 className="text-3xl font-extrabold p-5 text-center">Our Vision</h1>
                     <p>Welcome to Real Estate, a trusted name in the real estate industry committed
                         to helping individuals, families, and investors find the perfect property. With a strong
                         understanding of the local market and a passion for excellence, we deliver transparent,
@@ -45,6 +64,25 @@ function About() {
                         on trust and results.
                     </p>
                 </div>
+
+            </div>
+
+            <div className="border-1 rounded-2xl my-10 mx-5 bg-red 500 max-w-aotu
+             flex flex-row gap-5 justify-self-center">
+                <img src={ContactUs} className="w-80 h-60 my-10 mx-5" />
+
+                <div className="item-right flex flex-col p-5 bg-red-800/50">
+
+                    <h1 className="text-3xl font-extrabold p-5 text-center">Get In Touch with Us !</h1>
+                    <p className="text-lg">Whether you are looking to buy your dream home, sell your property,
+                         or invest in real estate, RealEstate is here to help. <br /> <br />
+
+                        📞 Phone: +91 9172896214 <br />
+                        📧 Email: ashishsmathpati@gmail.com <br />
+                        📍 Location: RealEstate, Wagholi, Pune <br />
+                    </p>
+                </div>
+
             </div>
         </div>
     </>
