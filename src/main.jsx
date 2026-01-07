b38e905cea24f001cf57f4fbd13659541829fe03
AshishSMathpati <ashishsmathpati@gmail.com>
2026-01-07 20:45:27 +0530

    route added

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ createRoot(document.getElementById('root')).render(
 <Routes>
     <Route  path='/' element={<Home/>} />
     <Route  path='/about' element={<About/>} />
+    <Route  path='/property' element={<Property/>} />
 </Routes>
 </BrowserRouter>
 
