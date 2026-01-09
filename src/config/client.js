8711cca0f38c3245e4c81b7c212629d4f0ff2ee5
AtharvBolke99 <atharvbolke99@gmail.com>
2026-01-09 18:00:50 +0530

    minor changes done

--- src/config/client.js
+++ src/config/client.js
@@ -2,21 +2,21 @@ const clientArray = [
     {
       title:
         "The attention to detail and personalized service was exceptional. They found us a stunning villa that exceeded all our expectations.Thank you, RealEstate!",
-      img: "https://avatar.iran.liara.run/public/25",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvZRzCOTmTpG-0zKoHeoNr8J-LeI_ihfZO3Q&s",
       name: "Pratik Salunke",
       role: "Luxury Home Buyer",
     },
     {
       title:
         "RealEstate made finding our dream home so easy. Their agents were patient, knowledgeable, and truly understood what we were looking for.",
-      img: "https://avatar.iran.liara.run/public/40",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuXEGCZl1QFq0tSzG7eWbLydP8N69jLNS8Ag&s",
       name: "Atharv Bolke",
       role: "First-time Buyer",
     },
     {
       title:
         "As an investor, I need reliable partners. RealEstate has consistently delivered excellent properties with great ROI. Their market insights are invaluable.",
-      img: "https://avatar.iran.liara.run/public",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFpMZnvOyb9sdqE7jmOL5PaBa83i0cSuH6Qw&s",
       name: "Ashish Mathpati",
       role: "Property Investor",
     },
